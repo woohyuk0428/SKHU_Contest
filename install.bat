e90a74f665f743dbe8ae5e2b8e53436ff3a3b4da
김현우 <khuoo4603@gmail.com>
2023-07-17 13:03:04 +0900

    ejs 수정

---
+++ install.bat
@@ -0,0 +1,11 @@
+REM vscode 터미널에서 파일경로를 적어서 실행 - ex) C:\NodeJs_Test\install.bat
+
+call npm install --save express
+
+call npm install nodemon -g
+
+call npm install ejs
+
+call npm install cookie-parser
+
+echo ALL Install.
--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require("express"); // express 프레임워크
 const request = require("request"); // request 모듈
+const cookieParser = require("cookie-parser");
+const fs = require("fs"); // fs 모듈
 const app = express(); //express 객체 생성
 const port = 8080; // 포트 번호 지정
 const today = new Date(); // 서버 오픈 시 기록용 현재 시간 저장
 
-const key = "72594c566a77686b3536686666694f"; // 지하철 API 키값 저장 - 추후 보안 업데이트 필요
-var url = "http://openapi.seoul.go.kr:8088/sample/json/CardSubwayStatsNew/1/5/20220301"; // 오류 방지용으로 임시 url 지정
+const key = fs.readFileSync("APIKey.txt", "utf8"); // 지하철 API 키값 저장
 
-// body-parser 옵션 지정
-app.use(express.json()); // json형태로 body 파싱
-app.use(express.urlencoded({ extended: false })); // 파싱 할 때 querystring모듈 사용
-
-// static으로 html과 같이 전송할 파일 경로 지정
-// #region
+// #region 파일 경로 지정, 옵션 설정
 app.use(express.static("static/image"));
 app.use(express.static("static/css"));
 app.use(express.static("static/javascript"));
-app.use(express.static("template"));
+
+app.use(express.json()); // json형태로 body 파싱
+app.use(express.urlencoded({ extended: false })); // 파싱 할 때 querystring모듈 사용
+
+app.set("view engine", "ejs"); // 뷰 엔진을 ejs로 설정
+app.set("views", "./views"); // 뷰 파일 경로 지정
+
+app.use(cookieParser("SHKU_Cookie")); // 쿠키 시크릿 키 지정
+
+// const cookieConfig = {
+//     httpOnly: true,
+//     maxAge: 500000,
+//     signed: true,
+//     httpOnly: false
+// };
 // #endregion
 
 //서버 오픈시 실행되는 함수. 현재 시간과 함께 서버가 실행됨(Mon Jul 03 2023 21:23:13 GMT+0900 (대한민국 표준시) | server reload)
@@ -25,71 +35,213 @@ app.listen(port, () => {
     console.log("http://localhost:8080/");
 });
 
-// Get라우팅 - 클라이언트로 기본 html 파일 전송
+// #region Get라우팅 - 클라이언트로 기본 html 파일 전송
 //http://localhost:8080/ 경로로 요청 시 Main.html파일 반환
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/template/Main.html");
+    res.render("Main");
 });
 
 //http://localhost:8080/halfway 경로로 요청 시 Halfway.html파일 반환
 app.get("/halfway", (req, res) => {
-    res.sendFile(__dirname + "/template/Halfway.html");
+    res.render("Halfway");
 });
 
 //http://localhost:8080/mapping 경로로 요청 시 Mapping.html파일 반환
 app.get("/mapping", (req, res) => {
-    res.sendFile(__dirname + "/template/Mapping.html");
+    res.render("Mapping");
 });
 
 //http://localhost:8080/suggestion 경로로 요청 시 Suggestion.html파일 반환
 app.get("/suggestion", (req, res) => {
-    res.sendFile(__dirname + "/template/Suggestion.html");
+    res.render("Suggestion");
 });
 
-//http://localhost:8080/suggestion 경로로 요청 시 Suggestion.html파일 반환
+//http://localhost:8080/post 경로로 요청 시 Suggestion.html파일 반환
 app.get("/post", (req, res) => {
-    res.sendFile(__dirname + "/template/Post.html");
+    // 쿠키가 있는지 검사
+    if (req.cookies.My_Station == undefined) {
+        var before = []; // 쿠키가 없으면 빈 배열을 반환
+        console.log(before);
+    } else {
+        var before = req.cookies.My_Station; // 쿠키가 있다면 쿠키값을 반환
+        Array.isArray(before) ? null : (before = [before]); // 값이 1개면 문자열을 배열로 변경
+        console.log(before);
+    }
+
+    res.render("Post", {
+        result_Data: "",
+        before_Data: before,
+    });
 });
+
+//쿠키 삭제
+app.get("/clearCookie", (req, res) => {
+    res.clearCookie("My_Station");
+
+    res.send("쿠키를 삭제하였습니다.");
+});
+
+app.get("/co", (req, res) => {
+    res.cookie("My_Station", ["서울", "부천", "부산"]);
+    res.send("쿠키를 생성하였습니다.");
+});
+
+//http://localhost:8080/XSS 경로로 요청 시 Suggestion.html파일 반환
+app.get("/XSS", (req, res) => {
+    res.render("XSS", {
+        result_Data: "",
+    });
+});
+// #endregion
 
 // http://localhost:8080/post - post라우팅
 app.post("/post", (req, res) => {
+    var s_response = req.body.station.replace(/\<|\>|\"|\'|\%|\;|\(|\)|\&|\+|\-/g, ""); // XSS 공격 방어
+
+    // ""입력시 현재 운행중인 모든 역이 나오기 때문에 이를 방지
+    if (s_response == "") {
+        if (req.cookies.My_Station == undefined) {
+            var before = []; // 쿠키가 없으면 빈 배열을 반환
+        } else {
+            var before = req.cookies.My_Station; // 쿠키가 있다면 쿠키값을 반환
+            Array.isArray(before) ? null : (before = [before]); // 값이 1개면 문자열을 배열로 변경
+        }
+
+        res.render("post", {
+            result_Data: "<script>alert('지하철 역 이름을 입력해주세요.')</script>",
+            before_Data: before,
+        });
+        return 0;
+    }
+
+    // 마지막 글자가 "역"이면 역을 삭제함
+    if (s_response.slice(-1) == "역") {
+        s_response = s_response.slice(0, -1);
+    }
+
     // 지하철 API에서 가져올 데이터 - url수정
-    url = "http://swopenapi.seoul.go.kr/api/subway/" + key + "/json/realtimeStationArrival/0/10/" + encodeURI(req.body.station);
+    const url = "http://swopenapi.seoul.go.kr/api/subway/" + key + "/json/realtimeStationArrival/0/10/" + encodeURI(s_response);
     console.log(url);
-    // url에 GET으로 request요청
     request(
         {
             url: url,
             method: "GET",
         },
         function (error, response, body) {
-            const obj = JSON.parse(body); // body의 데이터는 string으로 전송되기 때문에 json형식으로 변환
-            const data = obj.realtimeArrivalList[0]; // 필요한 데이터 경로 압축
-            console.log(data);
+            try {
+                const obj = JSON.parse(body); // body의 데이터는 string으로 전  송되기 때문에 json형식으로 변환
+                const data = obj.realtimeArrivalList[0]; // 필요한 데이터 경로 압축
 
-            // post페이지로 결과 반환
-            res.send(`<h1>사용자 님이 검색하신"${req.body.station}"에 대한 검색 결과입니다.</h1>
-            <p>열차 번호: ${data.subwayId}<br>
-            열차 방향: ${data.trainLineNm}<br>
-            열차 위치: ${data.arvlMsg2}<br><br>
-            열차 검색 시간은 "${data.recptnDt}"입니다.</p>`);
+                const arr = data.subwayList.split(","); // 문자열로 저장된 환승 정보를 배열로 변경
+                var Mystr = "";
+                arr.forEach((item, index) => {
+                    Mystr += `<span>환승 정보(${index + 1}):</span> ${item}<br>`; // 배열 요소만큼 문자열 생성
+                });
+
+                // post페이지로 결과 반환
+                var newHtml = `<br><h1>사용자님이 검색하신<span>"${s_response}"</span>에 대한 검색 결과입니다.</h1>
+                    <div class="result-info">
+                    <span>열차 노선:</span> ${data.subwayId}<br>
+                    <span>열차 번호:</span> ${data.btrainNo}<br>
+                    <span>열차 방향:</span> ${data.trainLineNm}<br>
+                    <span>열차 위치:</span> ${data.arvlMsg2}<br>
+                    ${Mystr}
+                    열차 검색 시간은 "${data.recptnDt}"입니다.</div>`;
+
+                // 쿠키가 존재하는지 확인
+                if (req.cookies.My_Station == undefined) {
+                    // 쿠키가 없을 때 실행
+                    res.cookie("My_Station", s_response); // 검색한 역을 쿠키 값으로 생성
+                    var MyCookie = [s_response]; // ejs에서 코드 실행 시 문자열이면 오류가 나기 때문에 배열로 변경
+                    console.log(`쿠키 없음: ${MyCookie}`);
+                } else {
+                    // 쿠키가 있을 때 실행
+                    var MyCookie = req.cookies.My_Station; // 쿠키값을 변수에 저장
+
+                    Array.isArray(MyCookie) ? null : (MyCookie = [MyCookie]); // 요소가 1개일 경우 문자열을 배열로 변경
+                    MyCookie.indexOf(s_response) + 1 ? MyCookie.splice(MyCookie.indexOf(s_response), 1) : null; // 중복된 요소가 있다면 삭제
+                    MyCookie.length >= 5 ? MyCookie.pop() : null; // 요소가 5개 이상이면 마지막 요소 삭제
+                    MyCookie.unshift(s_response); // 검색한 역을 배열 첫번째 요소로 추가
+
+                    res.cookie("My_Station", MyCookie); // 배열을 쿠키값으로 추가
+                    console.log(`쿠키 있음: ${MyCookie}`);
+                }
+
+                //결과 반환
+                res.render("post", {
+                    result_Data: newHtml,
+                    before_Data: MyCookie,
+                });
+            } catch (error) {
+                // 에러가 발생했을 때 예외 처리
+                console.error(error);
+                if (req.cookies.My_Station == undefined) {
+                    var before = []; // 쿠키가 없으면 빈 배열을 반환
+                } else {
+                    var before = req.cookies.My_Station; // 쿠키가 있다면 쿠키값을 반환
+                    Array.isArray(before) ? null : (before = [before]); // 값이 1개면 문자열을 배열로 변경
+                }
+
+                res.render("post", {
+                    result_Data: `<br><p style="color:red">"${s_response}"에 대한 검색 결과가 존재하지 않거나 데이터를 가져오는 중에 오류가 발생했습니다.</p>`,
+                    before_Data: before,
+                });
+            }
         }
     );
 });
 
-// 서울공공데이터 지하철API 테스트  (console.log)
-// #region
-app.get("/post/test", (req, res) => {
+// #region 서버 제작에서 주의해야 할 것, XSS 보안 취약점 - <script>alert("hi")</script>
+app.post("/XSS", (req, res) => {
+    var s_response = req.body.station;
+    if (s_response == "") {
+        res.render("post", {
+            result_Data: "<script>alert('지하철 역 이름을 입력해주세요.')</script>",
+        });
+        return 0;
+    }
+    if (s_response.slice(-1) == "역") {
+        s_response = s_response.slice(0, -1);
+    }
+
+    // 지하철 API에서 가져올 데이터 - url수정
+    const url = "http://swopenapi.seoul.go.kr/api/subway/" + key + "/json/realtimeStationArrival/0/10/" + encodeURI(s_response);
+    console.log(url);
     request(
         {
             url: url,
             method: "GET",
         },
         function (error, response, body) {
-            const obj = JSON.parse(body);
-            //console.log("Status", response.statusCode);
-            //console.log("Headers", JSON.stringify(response.headers));
-            console.log("Reponse received", obj.CardSubwayStatsNew.row[0].USE_DT);
+            try {
+                const obj = JSON.parse(body); // body의 데이터는 string으로 전송되기 때문에 json형식으로 변환
+                const data = obj.realtimeArrivalList[0]; // 필요한 데이터 경로 압축
+
+                const arr = data.subwayList.split(",");
+                var Mystr = "";
+                arr.forEach((item, index) => {
+                    Mystr += `환승 정보(${index + 1}): ${item}<br>`;
+                });
+
+                // post페이지로 결과 반환
+                var newHtml = `<br><h1>사용자 님이 검색하신"${s_response}"에 대한 검색 결과입니다.</h1>
+                    <p>열차 번호: ${data.subwayId}<br>
+                    열차 방향: ${data.trainLineNm}<br>
+                    열차 위치: ${data.arvlMsg2}<br>
+                    ${Mystr}
+                    열차 검색 시간은 "${data.recptnDt}"입니다.</p>`;
+
+                //결과 반환
+                res.render("XSS", {
+                    result_Data: newHtml,
+                });
+            } catch (error) {
+                // 에러가 발생했을 때 예외 처리
+                console.error(error);
+                res.render("XSS", {
+                    result_Data: `<br><p style="color:red">"${s_response}"에 대한 검색 결과가 존재하지 않거나 데이터를 가져오는 중에 오류가 발생했습니다.</p>`,
+                });
+            }
         }
     );
 });
