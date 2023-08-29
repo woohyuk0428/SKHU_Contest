a7725f2299384ef4e277b622dbe8055bee4dac2c
WooHyuk <101261573+woohyuk0428@users.noreply.github.com>
2023-08-29 22:53:15 +0900

    subway 구현
    api를 받아와서 역 검색시 해당 열차 노선별 열차 위치가 나오게 구현했습니다.

---
+++ router/R_Subway.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+const request = require("request");
+const fs = require("fs");
+const jsonFile = require("jsonfile");
+const lineData = jsonFile.readFileSync("./static/json/line.json");
+const key = fs.readFileSync("APIKey.txt", "utf8");
+const axios = require('axios');
+const req_url = "http://swopenapi.seoul.go.kr/api/subway/" + key + "/json/realtimeStationArrival/0/50/";
+const date =  new Date();
+const openapi_url = `https://openapi.kric.go.kr/openapi/convenientInfo/stationTimetable?serviceKey=${fs.readFileSync("timetableapi.txt","utf-8")}`;
+let dayCd = 0;
+
+function isWeekday(date) {
+    const day = date.getDay();
+    return day >= 1 && day <= 5;
+  }
+  
+  function isWeekend(date) {
+    const day = date.getDay();
+    return day === 0 || day === 6;
+  }
+  
+  if (isWeekday(date)) {
+    dayCd = 8;
+  } else if (isWeekend(date)) {
+    dayCd = 7;
+  } else {
+    dayCd = 9;
+  }  
+
+router.get("/", async (req, res) => {
+    let station = req.query.station;
+    try {
+
+        if (!station) {
+            station = "온수";
+        }
+        const url = req_url + encodeURI(station);
+        
+        const response = await axios.get(url);
+        const subwayArrivals = response.data.realtimeArrivalList;
+
+        const subwayInfo = {}; //json 변환
+        subwayArrivals.forEach(realtimeArrivalList => {
+            const subwayId = realtimeArrivalList.subwayId;
+            const subwayLine = lineData[subwayId];
+            if (!subwayInfo[subwayLine]) {
+                subwayInfo[subwayLine] = [];
+            }
+            const processedData = {
+                subwayId: subwayLine, // 호선
+                updnLine: realtimeArrivalList.updnLine, //상하행
+                trainLineNm : realtimeArrivalList.trainLineNm, //방향
+                statnNm: realtimeArrivalList.statnNm, //역 이름
+                btrainSttus: realtimeArrivalList.btrainSttus, // 열차 종류
+                btrainNo: realtimeArrivalList.btrainNo, // 열차번호
+                arvlMsg: realtimeArrivalList.arvlMsg2 //도착 message
+            };
+            subwayInfo[subwayLine].push(processedData);
+        });
+
+        res.render("Subway", {
+            result_Data: subwayInfo
+        });
+    } catch (error) {
+        console.error(error);
+        res.render("Subway", {
+            result_Data: `<br><p style="color:red">"${station}"에 대한 검색 결과가 존재하지 않거나 데이터를 가져오는 중에 오류가 발생했습니다.</p>`
+        });
+    }
+});
+
+module.exports = router;
