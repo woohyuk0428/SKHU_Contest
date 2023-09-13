65d43ca9f46a0251bcc8814a29724a32538ff605
Hwang, JaeHyeon <ten02130735@gmail.com>
2023-09-13 11:24:05 +0900

    기능 구현(사이드 토글)

--- static/javascript/Main.js
+++ static/javascript/Main.js
@@ -1,13 +1,12 @@
-const folding_bar = document.getElementById('folding-btn');
-
 new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 37.5665, lng: 126.978 }, // 초기 위치 설정
-    zoom: 12, // 확대/축소 레벨
+  center: { lat: 37.5665, lng: 126.978 }, // 초기 위치 설정
+  zoom: 12, // 확대/축소 레벨
 });
 
-function folding(obj){
-    const menu = document.getElementById('menu');
-    menu.style.left('-30%');
-}
+const foldingBtn = document.querySelector('.folding-btn');
+const menu = document.querySelector('.menu');
 
-folding_bar.addEventListener("click", folding)
+// 사이드 메뉴 토글
+foldingBtn.addEventListener('click', () => {
+menu.classList.toggle('hidden');
+});
