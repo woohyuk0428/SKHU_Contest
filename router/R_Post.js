4b49db1806c8b613f7ee1e5cef7e8b146a538078
WooHyuk <101261573+woohyuk0428@users.noreply.github.com>
2023-09-14 01:33:14 +0900

    feat: remove to tago_api and add to seoul_timetable

--- router/R_Post.js
+++ router/R_Post.js
@@ -4,9 +4,22 @@ const request = require("request"); // request 모듈
 const fs = require("fs"); // fs 모듈
 const jsonFile = require("jsonfile");
 
-const key = fs.readFileSync("./APIKey.txt","utf-8");
-const tago_key = fs.readFileSync("./tago_key.txt","utf-8");
+const key = fs.readFileSync("./APIKey.txt", "utf-8");
+const tago_key = fs.readFileSync("./tago_key.txt", "utf-8");
 
+//요일 구분해주는 function
+function getDayOfWeek() {
+    const now = new Date();
+    const dayOfWeek = now.getDay(); // 0은 일요일, 1은 월요일, ..., 6은 토요일
+  
+    if (dayOfWeek === 0) {
+      return '3';
+    } else if (dayOfWeek === 6) {
+      return '2';
+    } else {
+      return '1';
+    }
+  }
 //http://localhost:8080/post 경로로 요청 시 Suggestion.html파일 반환
 router.get("/", (req, res) => {
     // 쿠키가 있는지 검사
@@ -29,8 +42,8 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
     var s_response = req.body.station.replace(/\<|\>|\"|\'|\%|\;|\(|\)|\&|\+|\-/g, ""); // XSS 공격 방어
     var s_updnline = req.body.updnLine//상행 하행 구별
-    var s_line = req.body.SubwayLine;
-    console.log(s_line);
+    var s_line = req.body.SubwayLine; //노선 받는 변수
+    var stationNm = ""; //역코드 저장
 
 
     // ""입력시 현재 운행중인 모든 역이 나오기 때문에 이를 방지
@@ -50,13 +63,13 @@ router.post("/", (req, res) => {
     }
 
     // 마지막 글자가 "역"이면 역을 삭제함
-    if (s_response.slice(-1) == "역" && s_response != "서울역") {
+    if (s_response.slice(-1) == "역") {
         s_response = s_response.slice(0, -1);
     }
 
+    
     // 지하철 API에서 가져올 데이터 - url수정
-    const realarrive_url = "http://swopenapi.seoul.go.kr/api/subway/" + key + "/json/realtimeStationArrival/0/10/" + encodeURI(s_response); //seoul realtime url
-    const tago_api =`https://apis.data.go.kr/1613000/SubwayInfoService/getKwrdFndSubwaySttnList?serviceKey=${tago_key}&pageNo=1&numOfRows=10&_type=json&subwayStationName=${encodeURI(s_response)}`;//tago_api url
+    const realarrive_url = "http://swopenapi.seoul.go.kr/api/subway/" + key + "/json/realtimeStationArrival/0/20/" + encodeURI(s_response); //seoul realtime url
     const realTimePosition_url = `http://swopenapi.seoul.go.kr/api/subway/${key}/json/realtimePosition/0/50/${s_line}`
     request(
         {
@@ -68,10 +81,11 @@ router.post("/", (req, res) => {
                 const obj = JSON.parse(body); // body의 데이터는 string으로 전  송되기 때문에 json형식으로 변환
                 const data = obj.realtimeArrivalList[0]; // 필요한 데이터 경로 압축
                 const convert = jsonFile.readFileSync("./static/json/line.json");
+                
                 const subwayId = convert[data.subwayId];
                 const btrainNo = data.btrainNo; //열차번호
                 const trainLineNm = data.trainLineNm; //도착지방면
-                const arvlMsg2 =  data.arvlMsg2; //열차 도착 정보
+                const arvlMsg2 = data.arvlMsg2; //열차 도착 정보
                 const arvlMsg3 = data.arvlMsg3; //열차 도착 정보 2
                 const recptnDt = data.recptnDt; //열차 도착 시간
 
@@ -80,39 +94,59 @@ router.post("/", (req, res) => {
                 arr.forEach((item, index) => {
                     Mystr += `<span>환승 정보(${index + 1}):</span> ${convert[item]}<br>`; // 배열 요소만큼 문자열 생성
                 });
-                //api 데이터 json화
-                
-                //국토부 tago api 받아오는 request
+                // 역코드를 얻기 위한 작업
+                if(s_line === "1호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                else if(s_line === "2호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                else if(s_line === "3호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                else if(s_line === "4호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                else if(s_line === "5호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                else if(s_line === "6호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                else if(s_line === "7호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                else if(s_line === "8호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                else if(s_line === "9호선"){
+                    var station_line = jsonFile.readFileSync(`./static/json/Line/${s_line}.json`);
+                    stationNm = station_line[arvlMsg3];
+                }
+                //arvlMsg3에서 받은 역코드명으로 api 호출
+                const SearchSTNTimeTableByFRCodeService_url = `http://openapi.seoul.go.kr:8088/${key}/json/SearchSTNTimeTableByFRCodeService/1/500/${stationNm}/${getDayOfWeek()}/${s_updnline}/`;
                 request({
-                    url : tago_api,
-                    method: "get",
-                },
-                function (err, res, body){
-                    const data = JSON.parse(body);
-                    let sugDataResult = data["response"]["body"]["items"]["item"];
-                            var subwayStationId = "";
-            
-                            console.log(sugDataResult);
-                            sugDataResult.forEach((data, idx) => {
-                                if (sugDataResult[idx]["subwayStationName"] == s_response && sugDataResult[idx]["subwayRouteName"] == s_line) {
-                                    subwayStationId = sugDataResult[idx]["subwayStationId"];
-                                    console.log("subwayStationId값: ", subwayStationId);
-                                }
-                            });
-                const tago_timetable= `https://apis.data.go.kr/1613000/SubwayInfoService/getSubwaySttnAcctoSchdulList?serviceKey=${tago_key}&pageNo=1&numOfRows=500&_type=json&subwayStationId=${subwayStationId}&dailyTypeCode=01&upDownTypeCode=${s_updnline}`    
-                            request({
-                                url: tago_timetable,
-                                method: "GET",
-                            },
-                            function(err, res, body){
-                                console.log(tago_timetable);
-                                console.log("tago_timetable: "+JSON.parse(body));
-                            })
-
-            
-                });
-
-
+                    url: SearchSTNTimeTableByFRCodeService_url,
+                    method: "GET",
+                }, function(err, res, body){
+                    if(err){
+                        console.error(err);
+                    }
+                    try{
+                        const obj = JSON.parse(body);
+                    }catch(e){
+                        console.log(`error: ${e}`);
+                    }
+                    
+                })
                 // post페이지로 결과 반환
                 var newHtml = `<br><h1>${subwayId} ${s_response}역 결과입니다.</h1>
                     <div class="result-info">
@@ -166,3 +200,26 @@ router.post("/", (req, res) => {
 });
 
 module.exports = router;
+ // const tago_api = `https://apis.data.go.kr/1613000/SubwayInfoService/getKwrdFndSubwaySttnList?serviceKey=${tago_key}&pageNo=1&numOfRows=10&_type=json&subwayStationName=${encodeURI(arvlMsg3)}`;//tago_api url
+                // console.log(tago_api);
+
+                // //국토부 tago api 받아오는 request
+                // request({
+                //     url: tago_api,
+                //     method: "get",
+                // },
+                //     function (err, res, body) {
+                //         const data = JSON.parse(body);
+                //         let sugDataResult = data["response"]["body"]["items"]["item"];
+                //         var subwayStationId = "";
+
+                //         console.log(sugDataResult);
+                //         sugDataResult.forEach((data, idx) => {
+                //             if (sugDataResult[idx]["subwayStationName"] == arvlMsg3 && sugDataResult[idx]["subwayRouteName"] == s_line) {
+                //                 subwayStationId = sugDataResult[idx]["subwayStationId"];
+                //                 console.log("subwayStationId값: ", subwayStationId);
+                //             }
+                //         });
+                        
+
+                //     });
