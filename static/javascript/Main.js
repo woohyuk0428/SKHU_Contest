065925da9d09bf07bc7513722e2ab19195e76936
nmmn <jjj4120@gmail.com>
2023-09-16 21:05:08 +0900

    add
    클릭 시 다른 섹션으로 바뀜

--- static/javascript/Main.js
+++ static/javascript/Main.js
@@ -11,8 +11,43 @@ foldingBtn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
 
+// 길찾기 , 중간지점 , 지하철 이동 버튼
+const gotoMappingBtn = document.querySelector('#gotoMappingBtn');
 const gotoHalfWayBtn = document.querySelector('#gotoHalfWayBtn');
+const gotoSubwayBtn = document.querySelector('#gotoSubwayBtn');
 
-function goto() {
-  
-}
+const section1 = document.querySelector('.mapping-section');
+const section2 = document.querySelector('.halfway-section');
+const section3 = document.querySelector('.subway-section');
+
+gotoMappingBtn.addEventListener('click', () => {
+  section1.classList.remove('hidden');
+  section2.classList.add('hidden');
+  section3.classList.add('hidden');
+
+  // gotoMappingBtn.classList.add('on');
+  // gotoHalfWayBtn.classList.remove('on');
+  // gotoSubwayBtn.classList.remove('on');
+})
+
+gotoHalfWayBtn.addEventListener('click', () => {
+  section1.classList.add('hidden');
+  section2.classList.remove('hidden');
+  section3.classList.add('hidden');
+
+  // gotoMappingBtn.classList.remove('on');
+  // gotoHalfWayBtn.classList.add('on');
+  // gotoSubwayBtn.classList.remove('on');
+})
+
+gotoSubwayBtn.addEventListener('click', () => {
+  section1.classList.add('hidden');
+  section2.classList.add('hidden');
+  section3.classList.remove('hidden');
+
+  // gotoMappingBtn.classList.remove('on');
+  // gotoHalfWayBtn.classList.remove('on');
+  // gotoSubwayBtn.classList.add('on');
+})
+
+// 도움말 버튼
