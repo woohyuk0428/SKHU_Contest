267473065ef4a36856949c9204a7229088421adf
qkrehdwns <skhu202014128@gmail.com>
2023-09-20 12:21:35 +0900

    Mapping
    경로,검색 시 enter 오류

--- router/R_Mapping.js
+++ router/R_Mapping.js
@@ -1,11 +1,5 @@
 const nodeGeocoder = require("node-geocoder");
 const axios = require("axios");
-const express = require("express");
-
-
-const nodeGeocoder = require("node-geocoder");
-const axios = require("axios");
-
 const express = require("express"); // express 프레임워크
 
 const router = express.Router();
@@ -17,12 +11,12 @@ const geocoder = nodeGeocoder({
 
 //http://localhost:8080/mapping 경로로 요청 시 Mapping.html파일 반환
 router.get("/", (req, res) => {
-    res.render("Mapping");
     // 쿠키가 있는지 검사
     if (req.cookies.Destination == undefined) {
         var data = []; // 쿠키가 없으면 빈 배열을 반환
         console.log(data);
-    } else {
+    } 
+    else {
         var data = req.cookies.Destination; // 쿠키가 있다면 쿠키값을 반환
         Array.isArray(data) ? null : (data = [data]); // 값이 1개면 문자열을 배열로 변경
         console.log(data);
@@ -41,31 +35,31 @@ router.post("/", (req, res) => {
     res.json({ lat: latitude, lng: longitude }); //todo render가 없는 파일을 찾고 있어서 json데이터로 수정했습니다.
 });
 
-router.post("/destination",(req,res) =>{
+
+router.post("/destination",(req,res) =>{ 
     // 쿠키가 존재하는지 확인
     if (req.cookies.Destination == undefined) {
         res.cookie("Destination", data); // 검색한 역을 쿠키 값으로 생성
-        var Destination = [data]; // ejs에서 코드 실행 시 문자열이면 오류가 나기 때문에 배열로 변경
-        console.log(`쿠키 없음: ${Destination}`);
+        var destination = [data]; // ejs에서 코드 실행 시 문자열이면 오류가 나기 때문에 배열로 변경
+        console.log(`쿠키 없음: ${destination}`);
     } 
     else {
         // 쿠키가 있을 때 실행
         var data = req.cookies.Destination; // 쿠키값을 변수에 저장
+        var destination = [data];
+        Array.isArray(destination) ? null : (destination = [destination]); // 요소가 1개일 경우 문자열을 배열로 변경
+        destination.indexOf(data) + 1 ? destination.splice(destination.indexOf(data), 1) : null; // 중복된 요소가 있다면 삭제
+        destination.length >= 5 ? destination.pop() : null; // 요소가 5개 이상이면 마지막 요소 삭제
+        destination.unshift(data); // 검색한 역을 배열 첫번째 요소로 추가
 
-        Array.isArray(Destination) ? null : (Destination = [Destination]); // 요소가 1개일 경우 문자열을 배열로 변경
-        Destination.indexOf(data) + 1 ? Destination.splice(Destination.indexOf(data), 1) : null; // 중복된 요소가 있다면 삭제
-        Destination.length >= 5 ? Destination.pop() : null; // 요소가 5개 이상이면 마지막 요소 삭제
-        Destination.unshift(data); // 검색한 역을 배열 첫번째 요소로 추가
+        res.cookie("Destination", destination); // 배열을 쿠키값으로 추가
+        console.log(`쿠키 있음: ${destination}`);
 
-        res.cookie("Destination", Destination); // 배열을 쿠키값으로 추가
-        console.log(`쿠키 있음: ${Destination}`);
-    }
-
-    //결과 반환
-    res.render("post", {
-        Data: Destination,
-    });
-
+    } 
+        //결과 반환
+        res.render("post", {
+            Data: destination,
+        });
 });
 
 // 중간지점 찾기 버튼 누를 시 주소값 받아 데이터 가공
