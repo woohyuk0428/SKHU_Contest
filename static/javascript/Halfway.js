7bfc9d347d303b6eee73ef518ec05707d4df8ae0
woohyuk0428 <whkang03@gmail.com>
2023-10-25 03:43:43 +0900

    fix url

--- static/javascript/Halfway.js
+++ static/javascript/Halfway.js
@@ -456,7 +456,7 @@ function createMidMarkers(responseData, midpoint, map, marker_iconList, midconte
 async function H_fetchPlacePhoto(placeId) {
     const parser = new DOMParser();
 
-    const place_image_html = await fetch(`http://localhost:8080/Suggestion/PlacePhoto?placeId=${placeId}`);
+    const place_image_html = await fetch(`http://www.skhuroad.com/Suggestion/PlacePhoto?placeId=${placeId}`);
     const image_html = await place_image_html.json();
 
     let doc = parser.parseFromString(image_html.Html, "text/html");
